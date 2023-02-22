c7182ae9eed7432917fb17fd796ded071b79cf9d
rizaldi-itu <rizaldi.akbar@indotruck-utama.co.id>
2023-02-22 11:55:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const moment = require("moment");
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin : *");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
